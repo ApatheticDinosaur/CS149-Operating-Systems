0a92bd1a9993585ddeef3625041db1284a28257a
apatheticdinosaur <andy.wang@sjsu.edu>
2015-03-05 17:05:04 -0800

    finished ff, bf, and nf

--- CS149_HW4/src/Main.java
+++ CS149_HW4/src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -9,29 +10,43 @@ import java.util.Random;
  * The main to be run to start the simulation
  */
 public class Main {
-
-    ArrayList<Process> processes;
-
     public static void main(String [] args) {
-
-        int[] sizes = {5, 11, 17, 31};
-        ArrayList<Process> processes = new ArrayList<Process>();
-        Random r = new Random();
-
-        for (int i = 0; i < 100; i++) {
-            int size = sizes[r.nextInt(3)];
-            int duration = r.nextInt(4) + 1;
-            String name = "p" + i;
-            processes.add(new Process(size, duration, name));
-        }
-
-        for (Process p : processes) {
-            System.out.println("Name: "+ p.getName() + " Duration: " + p.getDuration() + " Size: " + p.getSize());
-        }
 
         PagingAlgorithms p = new PagingAlgorithms();
         SwappingAlgorithms s = new SwappingAlgorithms();
 
-        s.firstFit(processes);
+        System.out.println("----------------------------------------First Fit----------------------------------------");
+        double ffavg = 0;
+        for(int a = 0; a < 5; a++) {
+            ffavg += s.firstFit(createProcesses());
+        }
+        System.out.println("----------------------------------------Next Fit----------------------------------------");
+        double nfavg = 0;
+        for(int b = 0; b < 5; b++) {
+            nfavg += s.nextFit(createProcesses());
+        }
+        System.out.println("----------------------------------------Best Fit----------------------------------------");
+        double bfavg = 0;
+        for(int c = 0; c < 5; c++) {
+            bfavg += s.bestFit(createProcesses());
+        }
+
+        System.out.println("First fit average is " + ffavg / 5);
+        System.out.println("Next fit average is " + nfavg / 5);
+        System.out.println("Best fit average is " + bfavg / 5);
+    }
+
+    public static ArrayList<Process> createProcesses() {
+        ArrayList<Process> processes = new ArrayList<Process>();
+        int[] sizes = {5, 11, 17, 31};
+        Random r = new Random();
+        for (int i = 0; i < 150; i++) {
+            int size = sizes[r.nextInt(4)];
+            int duration = r.nextInt(5) + 1;
+            System.out.println(duration);
+            String name = Integer.toString(i);
+            processes.add(new Process(size, duration, name));
+        }
+        return processes;
     }
 }
--- CS149_HW4/src/SwappingAlgorithms.java
+++ CS149_HW4/src/SwappingAlgorithms.java
@@ -18,23 +18,18 @@ public class SwappingAlgorithms {
         }
     }
 
-    public void firstFit(ArrayList<Process> proc) {
-        int time = 0;
+    public void resetMemory() {
+        for(int i = 0; i < 100; i++) {
+            memory.set(i, ".");
+        }
+    }
+
+    public int firstFit(ArrayList<Process> proc) {
+        resetMemory();
         ArrayList<Process> processes = proc;
+        for (int time = 0; time < 60; time++) {
         for(Process p : processes) { // for each process
-            for (Process c : processes) {
-                if(memory.contains(c.getName())) { //decrease duration for each process in memory
-                    c.setDuration(c.getDuration() - 1);
-                }
-                if(c.getDuration() == 0) { //process is done, replace with .'s
-                    for(int i = 0; i < memory.size(); i++) {
-                        if(memory.get(i) == c.getName()) {
-                            memory.set(i, ".");
-                        }
-                    }
-                }
-            }
-            if (p.getDuration() == 0) {
+            if (p.getDuration() == 0 || memory.contains(p.getName())) {
                 continue;
             }
             int size = p.getSize();
@@ -44,7 +39,10 @@ public class SwappingAlgorithms {
                 if(memory.get(i) == ".") { //found empty spot, check next couple of spots
                     boolean available = true;
                     for (int j = i; j < size + i; j++) { //check if all slots are free
-                        if (memory.get(j) == null) { break; } //prevent index out of bound
+                        if (j == memory.size()) {
+                            available = false;
+                            break;
+                        } //prevent index out of bound
                         if (memory.get(j) != "."){ //another process is in the way, this process can't fit
                             available = false;
                         }
@@ -53,22 +51,159 @@ public class SwappingAlgorithms {
                         for (int j = i; j < size + i; j++) {
                             memory.set(j, name);
                         }
-                        System.out.println(memory.toString());
+                        System.out.println("Process " + name + " has entered memory, its duration is " + duration + " and size is " + size);
+                        for (int k = 0; k < memory.size(); k++) {
+                            System.out.print(memory.get(k));
+                        }
+                        System.out.print("\n");
                         break;
                     }
                 }
             }
         }
+        for (Process c : processes) {
+            if(memory.contains(c.getName())) { //decrease duration for each process in memory
+                c.setDuration(c.getDuration() - 1);
+            }
+            if(c.getDuration() == 0 && memory.contains(c.getName())) { //process is done, replace with .'s
+                for(int i = 0; i < memory.size(); i++) {
+                    if(memory.get(i) == c.getName()) {
+                        memory.set(i, ".");
+                    }
+                }
+                System.out.println("Process " + c.getName() + " has left memory");
+            }
+        }
+        }
+        int numfinished = 0;
+        for (Process h: processes) {
+            if (h.getDuration() == 0) {
+                numfinished++;
+            }
+        }
+        return numfinished;
     }
 
-    public void nextFit(ArrayList<Process> p) {
-        ArrayList<Process> processes = p;
-        //TODO: Add next fit algorithm here
+    public int nextFit(ArrayList<Process> proc) {
+        resetMemory();
+        int position = 0;
+        ArrayList<Process> processes = proc;
+        for (int time = 0; time < 60; time++) {
+            for (Process c : processes) {
+                if(memory.contains(c.getName())) { //decrease duration for each process in memory
+                    c.setDuration(c.getDuration() - 1);
+                }
+                if(c.getDuration() == 0 && memory.contains(c.getName())) { //process is done, replace with .'s
+                    for(int i = 0; i < memory.size(); i++) {
+                        if(memory.get(i) == c.getName()) {
+                            memory.set(i, ".");
+                        }
+                    }
+                    System.out.println("Process " + c.getName() + " has left memory");
+                }
+            }
+            for(Process p : processes) { // for each process
+                if (p.getDuration() == 0 || memory.contains(p.getName())) {
+                    continue;
+                }
+                int size = p.getSize();
+                int duration = p.getDuration();
+                String name = p.getName();
+                for (int i = position; i < memory.size(); i++) { //find a spot for this process
+                    if(memory.get(i) == ".") { //found empty spot, check next couple of spots
+                        boolean available = true;
+                        for (int j = i; j < size + i; j++) { //check if all slots are free
+                            if (j == memory.size()) {
+                                available = false;
+                                position = 0;
+                                break;
+                            } //prevent index out of bound
+                            if (memory.get(j) != "."){ //another process is in the way, this process can't fit
+                                available = false;
+                            }
+                        }
+                        if(available) {
+                            for (int j = i; j < size + i; j++) {
+                                memory.set(j, name);
+                            }
+                            System.out.println("Process " + name + " has entered memory, its duration is " + duration + " and size is " + size);
+                            for (int k = 0; k < memory.size(); k++) {
+                                System.out.print(memory.get(k));
+                            }
+                            System.out.print("\n");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        int numfinished = 0;
+        for (Process h: processes) {
+            if (h.getDuration() == 0) {
+                numfinished++;
+            }
+        }
+        return numfinished;
     }
 
-    public void bestFit(ArrayList<Process> p) {
-        ArrayList<Process> processes = p;
-        //TODO: Add best fit algorithm here
+    public int bestFit(ArrayList<Process> proc) {
+        resetMemory();
+        ArrayList<Process> processes = proc;
+        for (int time = 0; time < 60; time++) {
+            for(Process p : processes) { // for each process
+                if (p.getDuration() == 0 || memory.contains(p.getName())) {
+                    continue;
+                }
+                int size = p.getSize();
+                int duration = p.getDuration();
+                String name = p.getName();
+                for (int i = 0; i < memory.size(); i++) { //find a spot for this process
+                    if(memory.get(i) == ".") { //found empty spot, check next couple of spots
+                        boolean available = true;
+                        for (int j = i; j < size + i; j++) { //check if all slots are free
+                            if (j == memory.size()) {
+                                available = false;
+                                break;
+                            } //prevent index out of bound
+                            if (memory.get(j) != "."){ //another process is in the way, this process can't fit
+                                available = false;
+                            }
+                        }
+                        if(available) {
+                            for (int j = i; j < size + i; j++) {
+                                memory.set(j, name);
+                            }
+                            System.out.println("Process " + name + " has entered memory, its duration is " + duration + " and size is " + size);
+                            for (int k = 0; k < memory.size(); k++) {
+                                System.out.print(memory.get(k));
+                            }
+                            System.out.print("\n");
+                            break;
+                        }
+                    }
+                }
+            }
+            for (Process c : processes) {
+                if(memory.contains(c.getName())) { //decrease duration for each process in memory
+                    c.setDuration(c.getDuration() - 1);
+                }
+                if(c.getDuration() == 0 && memory.contains(c.getName())) { //process is done, replace with .'s
+                    for(int i = 0; i < memory.size(); i++) {
+                        if(memory.get(i) == c.getName()) {
+                            memory.set(i, ".");
+                        }
+                    }
+                    System.out.println("Process " + c.getName() + " has left memory");
+                }
+            }
+        }
+        int numfinished = 0;
+        for (Process h: processes) {
+            if (h.getDuration() == 0) {
+                numfinished++;
+            }
+        }
+        return numfinished;
     }
 
 }
